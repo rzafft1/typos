dba760bdc9512eb0ad976010b116c994b3551803
Ryan Zafft <ryanzafft@Ryans-MBP.lan>
2024-10-08 00:09:37 -0600

    Your commit message

--- tmp.cpp
+++ tmp.cpp
@@ -89,7 +89,7 @@ void call_helpdesk(int client_tid) {
 
 void do_something(int tid) {
     while (true) {
-        int do_something_time = rand() % 351;
+        int do_something_time = rand() % 51;
         sleep(do_something_time);
         printf("\n+++++++++++++++++++\n<Client %d> I have a problem!!!\n+++++++++++++++++++\n", tid);
         call_helpdesk(tid);  // Client calls the helpdesk
